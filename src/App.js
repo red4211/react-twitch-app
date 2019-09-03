e1f2b085c73e5483857f9a338e2dcb01bd85bb60
red42 <mperepelitsa421@gmail.com>
2019-09-03 11:47:26 +0300

    upd 1

--- src/App.js
+++ src/App.js
@@ -11,7 +11,13 @@ class App extends Component {
         data: []
       },
       showList:true,
-      clips:false
+      clips:false,
+      search: "",
+      showStreamCard: false,
+      renderClips: false,
+      resultStream: [],
+      resultProfile:[],
+      gameName: ""
     }
   }
 
@@ -25,6 +31,60 @@ class App extends Component {
     .then(data => {
       this.setState({activeStreams: data})
     })
+    }
+
+    setSearch = (event)=>{
+    	this.setState({
+    		search: event.target.value
+    	})
+    }
+
+    runSearch = (event)=>{
+    	event.preventDefault();
+    	fetch("https://api.twitch.tv/helix/streams?user_login="+ this.state.search,
+    		{
+		      headers: {
+		        "Client-ID": "wzdibt6ncymm2c9nquwlpkn9hg58i2"
+		      }
+		    }
+    		)
+    	.then(response => response.json())
+    	.then(response => {
+    		if(response.data.length!==0){//stream online
+    			fetch("https://api.twitch.tv/helix/games?id="+response.data[0].game_id,
+    				{ headers: {"Client-ID": "wzdibt6ncymm2c9nquwlpkn9hg58i2"}})
+    			.then(response => response.json())
+    			.then(response => {
+    				if(response.data.length){
+    					this.setState({gameName: response.data[0].name})
+    				}
+    			})
+    			this.setState({showList:false, showStreamCard:true, resultStream: response.data, resultProfile: [], renderClips:false})
+    		}else{//stream offline, get profile instead
+    	fetch("https://api.twitch.tv/helix/users?login="+ this.state.search,
+    		{
+		      headers: {"Client-ID": "wzdibt6ncymm2c9nquwlpkn9hg58i2"}})
+    		.then(response => response.json())
+    		.then(response => {
+    			if(response.data.length===0){
+    				this.setState({resultStream:[], resultProfile:[], showStreamCard:true, showList:false, renderClips:false})
+    			}else{
+    				this.setState({resultProfile: response.data, showStreamCard:true, showList:false, resultStream: [], renderClips:false})
+    			}
+    		})
+    		}
+    	})
+    }
+
+    backFromSearch = (event)=>{
+    	this.setState({
+    		showStreamCard: false,
+    		showList: true,
+    		resultStream: [],
+    		resultProfile: [],
+    		gameName: "",
+    		search: ""
+    	})
     }
 
     showNext = (event)=>{
@@ -61,22 +121,26 @@ class App extends Component {
       }
     }).then(response => response.json())
     .then(data => {
-      this.setState({clips: data, showList:false})
+      this.setState({clips: data, showList:false, renderClips:true})
     })  
     }
 
     returnToList = ()=>{
-      this.setState({showList:true})
+      this.setState({showList:true, renderClips:false})
     }
 
      render(){
     return (
     <div className="App">
+      <Search propSearch={this.setSearch} searchString={this.state.search} runSearch={this.runSearch}/>
       <List list={this.state.activeStreams.data} hClick = {this.showNext} showPrev={this.showPrev} showClips={this.showClips} 
         showList={this.state.showList}
         clips = {this.state.clips}
         returnToList = {this.returnToList}
+        renderClips = {this.state.renderClips}
       />
+      <BroadcasterCard showStreamCard={this.state.showStreamCard} streamInfo={this.state.resultStream} profileInfo={this.state.resultProfile}
+      game={this.state.gameName} backFromSearch={this.backFromSearch}/>
     </div>
   );
   }
@@ -93,10 +157,10 @@ const list = this.props.list.map((current,index)=>{
 
       return(
           <li key={index}>
-          <p className="title">{current.title}</p>
-            <p className="username" >{current.user_name}</p>
-            <p className="viewCount">{current.viewer_count} viewers</p>
-            <img src={imgUrl} />
+            <a href={"https://www.twitch.tv/"+current.user_name} className="stream-link" target="blank"><img src={imgUrl} /></a>
+            <p className="title">{current.title}</p>
+            <p>{current.user_name}</p>
+            <p>{current.viewer_count} viewers</p>
             <button className="nav-button" onClick={(e)=>{this.props.showClips(current.user_id, e)}} >Top clips</button>
           </li>
         )
@@ -109,7 +173,7 @@ const list = this.props.list.map((current,index)=>{
           <ul className="streams-list">{list}</ul>
         </div>
       )
-    }else{//clips
+    }else if(this.props.renderClips){//clips
       const clipsList = this.props.clips.data.map((current,index)=>{
         return(
             <li key={index}>
@@ -130,8 +194,52 @@ const list = this.props.list.map((current,index)=>{
           <ul className="clips-list">{clipsList} </ul>
           </div>
         )
+    }else{
+    	return null;
     }
   }
+}
+
+function Search(props){
+	return(
+			<form onSubmit={props.runSearch} className="search-form" >
+				<input type="text" placeholder="Search" onChange={props.propSearch} value={props.searchString} className="search-field" />
+			</form>
+		)
+}
+
+function BroadcasterCard(props){
+
+	if(props.showStreamCard==false){
+		return null;
+	}else if(props.streamInfo.length!==0){//stream online
+		const streamDetails = props.streamInfo[0];
+		var imgUrl = streamDetails.thumbnail_url;
+		imgUrl = imgUrl.replace("{width}", "640");
+      imgUrl = imgUrl.replace("{height}", "360");
+		return(
+				<div className="result-stream" >
+					<button onClick={props.backFromSearch} className="nav-button" >Back</button>
+					<p className="nomt" >{streamDetails.user_name}</p>
+					<a href={"https://www.twitch.tv/"+streamDetails.user_name} target="blank" className="result-stream-link" ><img src={imgUrl} /></a>
+					<p>Category: {props.game}</p>
+					<p className="title">{streamDetails.title}</p>
+					<p>{streamDetails.viewer_count} viewers</p>
+				</div>
+			)
+	}else if(props.profileInfo.length!==0){
+		return(
+				<div className="result-profile">
+					<button onClick={props.backFromSearch} className="nav-button" >Back</button>
+					<a href={"https://www.twitch.tv/"+props.profileInfo[0].display_name} target="blank">
+						<img src={props.profileInfo[0].profile_image_url} />
+					<p>{props.profileInfo[0].display_name}</p>
+					</a>
+				</div>
+			)
+	}else{
+		return <div><button onClick={props.backFromSearch} className="nav-button" >Back</button><p>User not found</p> </div>
+	}
 }
 
 export default App;
