831b7fca45723ef65b14a1d894eeccfc50d13412
red42 <mperepelitsa421@gmail.com>
2019-09-04 13:28:28 +0300

    Updates

---
+++ precache-manifest.ac28c693767957d5e6a6652822b6b099.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6406f31401f45c426c87a63fe85cffe",
+    "url": "/react-twitch-app/index.html"
+  },
+  {
+    "revision": "25854a9154b2eb60ddf6",
+    "url": "/react-twitch-app/static/css/main.d1917899.chunk.css"
+  },
+  {
+    "revision": "ee9c192b35db66f42127",
+    "url": "/react-twitch-app/static/js/2.b6ee9460.chunk.js"
+  },
+  {
+    "revision": "25854a9154b2eb60ddf6",
+    "url": "/react-twitch-app/static/js/main.028217f3.chunk.js"
+  },
+  {
+    "revision": "6653661939e53926f4fd",
+    "url": "/react-twitch-app/static/js/runtime~main.dbcd0335.js"
+  }
+]);
